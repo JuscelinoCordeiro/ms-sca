9a5923d781fd9ffa3cef51f87367594a51e797ce
Juscelino Cordeiro <jfilhodev@gmail.com>
2021-03-02 06:08:30 -0300

    Update config.php

--- conexao/config.php
+++ conexao/config.php
@@ -4,6 +4,6 @@
     define('DB_HOST', '172.21.0.2');
 //    define('DB_HOST', 'localhost');
 //     define('DB_PASS', '@!@#rf');
-    define('DB_PASS', 'ch@r*EB');
-    define('DB_USER', 'dimitri');
+    define('DB_PASS', '');
+    define('DB_USER', '');
 ?>
